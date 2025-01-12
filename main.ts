88959f53ad49932b9fb7eec311dfffbf24fc1bae
Tim Cortesi <tcortesi@gmail.com>
2025-01-11 23:05:52 -0500

    Add serial logging of received data

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
 radio.onReceivedString(function (receivedString) {
-    wind_direction = receivedString.substr(3, 1)
+    wind_direction = receivedString.substr(3, 3)
+    serial.writeLine("dir:" + receivedString.substr(3, 3))
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "tmp") {
-        temperature = Math.round(value / 100 * (9 / 5) + 32)
+        temperature = Math.round(value)
+        serial.writeValue("tmp", value)
     } else if (name == "prs") {
         pressure = value
+        serial.writeValue("prs", value)
     } else if (name == "hum") {
         humidity = value
-    } else if (name == "alt") {
-        humidity = value
+        serial.writeValue("hum", value)
     } else if (name == "ran") {
         rain = value
+        serial.writeValue("ran", value)
     } else if (name == "spd") {
         wind_speed = Math.round(value)
+        serial.writeValue("spd", value)
     }
 })
 let rain = 0
@@ -22,6 +26,7 @@ let pressure = 0
 let wind_direction = ""
 let wind_speed = 0
 let temperature = 0
+serial.redirectToUSB()
 radio.setGroup(86)
 makerbit.connectLcd(39)
 makerbit.showStringOnLcd1602("Test", makerbit.position1602(LcdPosition1602.Pos1), 16)
@@ -29,9 +34,5 @@ while (true) {
     basic.showString("Temp")
     basic.showNumber(temperature)
     basic.showString("Wind")
-    basic.showString("" + (wind_speed))
-    basic.showString(wind_direction)
-    basic.pause(1000)
-    basic.clearScreen()
-    basic.pause(5000)
+    basic.showString("" + wind_speed + wind_direction)
 }
